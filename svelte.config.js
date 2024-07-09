b098aa367544cd4e85c022822174218fee168079
dlaiu <darryllaiu95@gmail.com>
2024-07-09 17:53:33 -0400

    updated config again

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,13 +9,38 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		appDir: 'app',
 		adapter: adapter({
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/john-oliver-narrative' : ''
+			base: process.env.NODE_ENV === 'production' ? '/dlaiu/john-oliver-narrative' : ''
+			// relative: false
 		}
-	}
+	},
+	preprocess: vitePreprocess(),
+
+
 };
 
 export default config;
+
+
+
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		}),
+// 		paths: {
+// 			base: process.env.NODE_ENV === 'production' ? '/dlaiu/john-oliver-narrative' : ''
+// 		}
+// 	}
+// };
+
+// export default config;
